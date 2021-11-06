f040d1041f15fe876249a19fcfbb36a818178e9d
kbanchi1111 <kbanchi.1111@gmail.com>
2021-11-06 10:25:49 +0900

    add omikujiapp

--- OmikujiApp/ViewController.swift
+++ OmikujiApp/ViewController.swift
@@ -14,6 +14,57 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var stickView: UIView!
+    
+    @IBOutlet weak var stickLabel: UILabel!
+    
+    @IBOutlet weak var stickHeight: NSLayoutConstraint!
+    
+    @IBOutlet weak var stickBottomMargin: NSLayoutConstraint!
+    
+    @IBOutlet weak var overView: UIView!
+    
+    @IBOutlet weak var bigLabel: UILabel!
+    
+    let resultTexts: [String] = [
+        "大吉",
+        "中吉",
+        "小吉",
+        "吉",
+        "末吉",
+        "凶",
+        "大凶"
+    ]
+    
+    /*
+     override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+         //
+     }
+     */
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        
+        if motion != UIEvent.EventSubtype.motionShake || overView.isHidden == false {
+            return
+        }
+        
+        let resultNum = Int(arc4random_uniform(UInt32(resultTexts.count)))
+        stickLabel.text = resultTexts[resultNum]
+        stickBottomMargin.constant = stickHeight.constant * -1
+        
+        UIView.animate(withDuration: 1.0, animations: {
+            self.view.layoutIfNeeded()
+        }, completion: {
+            (finished: Bool) in
+            self.bigLabel.text = self.stickLabel.text
+            self.overView.isHidden = false
+        })
+        
+    }
+    
+    @IBAction func tapRetryButton(_ sender: Any) {
+        overView.isHidden = true
+        stickBottomMargin.constant = 0
+    }
 }
 
